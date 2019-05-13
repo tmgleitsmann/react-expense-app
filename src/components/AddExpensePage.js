9d19ad5491b05de33ec3a5dc18ffee0ce066bf9b
Tom Gleitsmann <tomgleitsmann@Toms-MacBook-Air.local>
2019-05-13 15:49:02 -0700

    more revisions

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -4,29 +4,29 @@ import ExpenseForm from './ExpenseForm';
 import { addExpense } from '../actions/expenses';
 
 
-const ExpenseModel = require('../models/expenses');
+//const ExpenseModel = require('../models/expenses');
 
-const writeToDB = ({description}, {amount}, {createdAt}, {note}) => ({
-    let expense = new ExpenseModel({
-        description,
-        amount,
-        createdAt,
-        note
-    });
-    console.log('save to mongodb')
-    expense.save().then(doc => {
-        console.log(doc)
-    }).catch(err => {
-        console.error(err)
-    })
-});  
+// const writeToDB = ({description}, {amount}, {createdAt}, {note}) => ({
+//     let expense = new ExpenseModel({
+//         description,
+//         amount,
+//         createdAt,
+//         note
+//     });
+//     console.log('save to mongodb')
+//     expense.save().then(doc => {
+//         console.log(doc)
+//     }).catch(err => {
+//         console.error(err)
+//     })
+// });  
 
 const AddExpensePage = (props) => (
     <div>
         <h1>This is from my add expense component</h1>
         <ExpenseForm 
         onSubmit = {(expense) => { 
-            writeToDb(props.dispatch(addExpense(expense)));
+            props.dispatch(addExpense(expense));
             props.history.push('/'); 
         }}
         />
