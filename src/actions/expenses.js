f535d5e46879689eeaf94d61f13978e7d90f2901
tmgleitsmann <tmgleitsmann@gmail.com>
2019-05-20 19:48:29 -0700

    changed apiurl back to heroku

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -2,8 +2,8 @@ import uuid from 'uuid';
 import timestamp from 'time-stamp';
 import axios from 'axios';
 //import store from '../store/configureStore';
-const apiUrl = 'http://localhost:3000/api';
-//const apiUrl = 'https://gleitsmann-expense-app.herokuapp.com/api';
+//const apiUrl = 'http://localhost:3000/api';
+const apiUrl = 'https://gleitsmann-expense-app.herokuapp.com/api';
 
 //add expense
 export const addExpense = (expense) =>{
